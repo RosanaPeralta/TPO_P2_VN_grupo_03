426fc485e4891b73542c3b3403b9de504af441a4
Diego <disavoia@uade.edu.ar>
2024-04-27 14:24:23 -0300

    Se agregó la funcionalidad de "traza" para la QueueOfStacks en QueueOfStacksUtil

--- src/main/java/org/example/Ejercicio2_1/model/stack/QueueOfStacksUtil.java
+++ src/main/java/org/example/Ejercicio2_1/model/stack/QueueOfStacksUtil.java
@@ -2,29 +2,71 @@ package org.example.Ejercicio2_1.model.stack;
 
 import org.example.Ejercicio2_1.model.QueueOfStacks;
 import org.example.model.Queue;
+import org.example.model.Stack;
 
 public class QueueOfStacksUtil {
 
     public void traza(QueueOfStacks queueOfStacks) {
         QueueOfStacks aux1 = new QueueOfStacks();
         QueueOfStacks aux2 = new QueueOfStacks();
-        int count = 0;
+        int countColumns = 0;
+        int countRows = 0;
+        int maxRowElements = 0;
+        int traza = 0;
+
+        // Caso Crítico: No hay elementos en la matríz
 
         if(queueOfStacks.isEmpty()) {
             throw new RuntimeException("No se puede calcular la traza de una cola vacía");
         }
 
         while(!queueOfStacks.isEmpty()){
-            aux1.addStack(queueOfStacks.getFirst());
-            aux2.addStack(queueOfStacks.getFirst());
+            aux1.addStack(copyStack(queueOfStacks.getFirst()));
+            aux2.addStack(copyStack(queueOfStacks.getFirst()));
             queueOfStacks.removeStack();
-            count++;
+            countColumns++;
         }
         while (!aux1.isEmpty()) {
             queueOfStacks.addStack(aux1.getFirst());
             aux1.removeStack();
         }
-        System.out.println(count);
+
+        while(!aux2.isEmpty()){
+            Stack row = aux2.getFirst();
+            aux1.addStack(aux2.getFirst());
+            aux2.removeStack();
+
+            while(!row.isEmpty()){
+                row.remove();
+                countRows++;
+            }
+
+            if(countRows > maxRowElements){
+                maxRowElements = countRows;
+            }
+
+            countRows = 0;
+
+        }
+
+        // Caso Crítico: Matriz no simétrica
+
+        if (maxRowElements != countColumns){
+            throw new RuntimeException("No se puede calcular la traza de una matriz no simétrica");
+        }
+
+        for(int i = 0; i <= countColumns-1; i++){
+            QueueOfStacks aux = copy(queueOfStacks);
+            for(int j = 0; j < countColumns-1-i; j++){
+                aux.removeStack();
+            }
+            Stack row = copyStack(aux.getFirst());
+            for(int k = 0; k < i; k++){
+                row.remove();
+            }
+            traza += row.getTop();
+        }
+        System.out.println("La traza de la matriz es: " + traza);
 
     }
 
@@ -35,4 +77,57 @@ public class QueueOfStacksUtil {
     public void sumaMatricial(Queue queueOfStacks) {
 
     }
+
+    public static Stack copyStack(Stack stack){
+        Stack stack1= new Stack();
+        Stack stack2= new Stack();
+
+        while (!stack.isEmpty()){
+            stack1.add(stack.getTop());
+            stack2.add(stack.getTop());
+            stack.remove();
+        }
+        while(!stack1.isEmpty()){
+            stack.add(stack1.getTop());
+            stack1.remove();
+        }
+        while(!stack2.isEmpty()){
+            stack1.add(stack2.getTop());
+            stack2.remove();
+        }
+        return stack1;
+    }
+
+    public static Queue copyQueue(Queue queue){
+        Queue queue1= new Queue();
+        Queue queue2= new Queue();
+
+        while (!queue.isEmpty()){
+            queue1.add(queue.getFirst());
+            queue2.add(queue.getFirst());
+            queue.remove();
+        }
+        while(!queue1.isEmpty()){
+            queue.add(queue1.getFirst());
+            queue1.remove();
+        }
+        return queue2;
+    }
+
+    public static QueueOfStacks copy(QueueOfStacks queueOfStacks){
+        QueueOfStacks aux= new QueueOfStacks();
+        QueueOfStacks aux2= new QueueOfStacks();
+
+        while(!queueOfStacks.isEmpty()){
+            aux.addStack(copyStack(queueOfStacks.getFirst()));
+            aux2.addStack(copyStack(queueOfStacks.getFirst()));
+            queueOfStacks.removeStack();
+        }
+        while(!aux.isEmpty()){
+            queueOfStacks.addStack(aux.getFirst());
+            aux.removeStack();
+        }
+        return aux2;
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,19 +10,28 @@ public class Main {
         Stack stack1 = new Stack();
         Stack stack2 = new Stack();
         Stack stack3 = new Stack();
+        Stack stack4 = new Stack();
 
-        stack1.add(1);
+        stack1.add(8);
         stack1.add(4);
         stack1.add(7);
+        stack1.add(3);
+
 
         stack2.add(2);
-        stack2.add(5);
-        stack3.add(8);
-
+        stack2.add(3);
+        stack2.add(8);
+        stack2.add(7);
 
         stack3.add(3);
         stack3.add(6);
-        stack3.add(9);
+        stack3.add(0);
+        stack3.add(1);
+
+        stack4.add(3);
+        stack4.add(8);
+        stack4.add(2);
+        stack4.add(5);
 
 
         QueueOfStacks queueOfStacks = new QueueOfStacks();
@@ -31,11 +40,9 @@ public class Main {
         queueOfStacks.addStack(stack1);
         queueOfStacks.addStack(stack2);
         queueOfStacks.addStack(stack3);
+        queueOfStacks.addStack(stack4);
 
         queueOfStacks12.traza(queueOfStacks);
-        queueOfStacks.removeStack();
-        queueOfStacks12.traza(queueOfStacks);
-
 
     }
 }
