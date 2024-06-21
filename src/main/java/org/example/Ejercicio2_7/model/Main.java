17d0269ed3ea7a22b53b981a0dd1219b3fe709d1
Diego <disavoia@uade.edu.ar>
2024-06-21 10:50:34 -0300

    Agregado ejercicio 2.7

--- src/main/java/org/example/Ejercicio2_7/model/Main.java
+++ src/main/java/org/example/Ejercicio2_7/model/Main.java
@@ -1,4 +1,25 @@
 package org.example.Ejercicio2_7.model;
 
+import org.example.model.definition.PriorityQueue;
+import org.example.model.dynamic.DynamicPriorityQueue;
+
 public class Main {
+    public static void main(String[] args) {
+        PriorityQueue cola = new DynamicPriorityQueue();
+        cola.add(1,1);
+        cola.add(2,1);
+        cola.add(3,1);
+        cola.add(4,2);
+        cola.add(5,2);
+        cola.add(6,2);
+        cola.add(7,3);
+        cola.add(8,3);
+        cola.add(9,3);
+
+        System.out.println(PriorityQueueUtil.print(cola));
+        PriorityQueue colaEditada = PriorityQueueUtil.edit(cola,6,2,2);
+        System.out.println(PriorityQueueUtil.print(colaEditada));
+
+    }
+
 }
