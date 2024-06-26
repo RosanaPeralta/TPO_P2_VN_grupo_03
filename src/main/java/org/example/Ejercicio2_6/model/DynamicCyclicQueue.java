b891834aa35ac2f717392b0eede8b251fe2c41b7
martincho5 <40181060+martincho5@users.noreply.github.com>
2024-06-25 21:04:34 -0300

    Se agrega ejercicio del 2.3.3 y el 2.6.2 y 2.6.3

---
+++ src/main/java/org/example/Ejercicio2_6/model/DynamicCyclicQueue.java
@@ -0,0 +1,44 @@
+package org.example.Ejercicio2_6.model;
+
+import org.example.model.definition.Queue;
+
+public class DynamicCyclicQueue implements Queue {
+
+    private NodeDouble first;
+
+    @Override
+    public void add(int a) {
+        if(this.isEmpty()){
+            this.first= new NodeDouble(a, null, null);
+            this.first.setNext(this.first);
+            this.first.setBefore(this.first);
+            return;
+        }
+        NodeDouble nuevo= new NodeDouble(a, this.first.getBefore(), this.first);
+        this.first.getBefore().setNext(nuevo);
+        this.first.setBefore(nuevo);
+        this.first=nuevo;
+    }
+
+    @Override
+    public void remove() {
+        if(this.isEmpty()){
+            throw new RuntimeException("La cola está vacía");
+        }
+        NodeDouble nodo= this.first.getBefore();
+        nodo.getBefore().setNext(nodo.getNext());
+        nodo.getNext().setBefore(nodo.getBefore());
+        nodo.setNext(null);
+        nodo.setBefore(null);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.first==null;
+    }
+
+    @Override
+    public int getFirst() {
+        return this.first.getBefore().getValue();
+    }
+}
